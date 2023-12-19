b24ae02e5da5b9a90d41e932752243edbe06a09c
Jaryllan <32004583+Jaryllan@users.noreply.github.com>
2023-12-19 14:07:02 +0800

    Update WeatherForecastController.cs

--- test-repo/Controllers/WeatherForecastController.cs
+++ test-repo/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace testrepo.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("Test 1.");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
